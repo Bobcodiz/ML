fc674d78810221295927da056c4915474e8aec40
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-08-29 22:49:57 +0300

    some logistic regression

--- LinearRegression/diabetes.py
+++ LinearRegression/diabetes.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.datasets import load_diabetes
 
-
 diabetes = load_diabetes()
 print(diabetes)
 diabetes = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
